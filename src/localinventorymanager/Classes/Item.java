0b384301857a417b65a336b1ad3a5f6593d972b9
Xenire <yayos21@outlook.es>
2025-09-07 18:35:41 -0600

    Delete Employee Class unnecesary for current scope

--- src/localinventorymanager/Classes/Item.java
+++ src/localinventorymanager/Classes/Item.java
@@ -29,9 +29,6 @@ public class Item {
     private String imagePath;
     private int stockBound;
     private String supplier;
-    private Employee lastMod;
-    private LocalDateTime lastModTime;
-    
     private Item(Builder builder){
         this.name=builder.name;
         this.description=builder.description;
